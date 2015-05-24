d5f1b5f03a38310cd9bf8f7fd43ecf5a2a1ed16c
simonenkong <simonenkong@yandex.ru>
2015-05-24 10:51:26 +0300

    GroupHelper/ContactHelper added

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_new_contact(Contact(firstname="Наталия", middlename="Георгиевна", lastname="Симоненко", nickname="simon",
+    app.contact.add_new(Contact(firstname="Наталия", middlename="Георгиевна", lastname="Симоненко", nickname="simon",
                          title="title1", company="company1", address="address1",
                          home="home1", mobile="mobile1", work="work1", fax="fax1", email="simonenkong@company1", email2="simon2@campany2",
                          email3="simon3@company3", homepage="homepage1", byear="1989", ayear="2007", address2="address2", phone2="phone2", notes="notes2"))
