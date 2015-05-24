5cfb3f7f5c03955be3e8f0812be8d24e936b680e
simonenkong <simonenkong@yandex.ru>
2015-05-24 11:35:04 +0300

    Edit group test added

--- fixture/group.py
+++ fixture/group.py
@@ -41,3 +41,26 @@ class GroupHelper:
         # submit group deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # find first group
+        wd.find_element_by_name("selected[]").click()
+        # submit first group edit
+        wd.find_element_by_name("edit").click()
+        # edit group page
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit groups update
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+
---
+++ test/test_edit_group.py
@@ -0,0 +1,9 @@
+__author__ = 'Nataly'
+
+from model.group import Group
+
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="edited_name", header="edited_header", footer="edited_footer"))
+    app.session.logout()
