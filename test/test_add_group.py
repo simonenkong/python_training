e8871068fff068af98761c3d45057f8fcff8705f
simonenkong <simonenkong@yandex.ru>
2015-05-24 10:38:04 +0300

    SessionHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,13 +11,13 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="name1", header="header1", footer="footer1"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
