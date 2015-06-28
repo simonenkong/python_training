5a022d2e2637efeb0cfd58efb3dec9831c16fb70
simonenkong <simonenkong@yandex.ru>
2015-06-28 16:40:40 +0300

    ORM added

--- check_db_connector.py
+++ check_db_connector.py
@@ -1,12 +1,13 @@
 __author__ = 'Nataly'
-from fixture.db import DbFixture
+from fixture.orm import ORMFixture
+from model.group import Group
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-    groups = db.get_contact_list()
-    for group in groups:
-        print(group)
-    print(len(groups))
+    l = db.get_contacts_not_in_group(Group(id="64"))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    db.destroy()
+    pass #db.destroy()
