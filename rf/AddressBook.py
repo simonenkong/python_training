d65c2d6f76b6f5643fb51c852916adabfa452aaf
simonenkong <simonenkong@yandex.ru>
2015-07-19 20:28:48 +0300

    RF for contacts

--- rf/AddressBook.py
+++ rf/AddressBook.py
@@ -5,6 +5,7 @@ import json
 import os.path
 from fixture.db import DbFixture
 from model.group import Group
+from model.contact import Contact
 
 
 class AddressBook:
@@ -31,14 +32,34 @@ class AddressBook:
     def get_group_list(self):
         return self.dbfixture.get_group_list()
 
+    def get_contact_list(self):
+        return self.dbfixture.get_contact_list()
+
     def new_group(self, name, header, footer):
         return Group(name=name, header=header, footer=footer)
+
+    def new_contact(self, firstname, lastname):
+        return Contact(firstname=firstname, lastname=lastname)
 
     def create_group(self, group):
         self.fixture.group.create(group)
 
+    def create_contact(self, contact):
+        self.fixture.contact.add_new(contact)
+
     def delete_group(self, group):
         self.fixture.group.delete_group_by_id(group.id)
 
+    def delete_contact(self, contact):
+        self.fixture.contact.delete_contact_by_id(contact.id)
+
+    def change_contact_name(self, contact, new_name):
+        edited_contact = contact
+        edited_contact.firstname = new_name
+        self.fixture.contact.edit_contact_by_id(edited_contact, contact.id)
+
     def group_lists_should_be_equal(self, list1, list2):
         assert sorted(list1, key=Group.id_or_max) == sorted(list2, key=Group.id_or_max)
+
+    def contact_lists_should_be_equal(self, list1, list2):
+        assert sorted(list1, key=Contact.id_or_max) == sorted(list2, key=Contact.id_or_max)
--- rf/group_scenarios.robot
+++ rf/group_scenarios.robot
@@ -22,3 +22,30 @@ Delete group
     ${new_list}=  Get Group List
     Remove Values From List  ${old_list}  ${group}
     Group Lists Should Be Equal  ${old_list}  ${new_list}
+
+Add new contact
+    ${old_list}=  Get Contact List
+    ${contact}=  New Contact  firstname1  lastname1
+    Create Contact  ${contact}
+    ${new_list}=  Get Contact List
+    Append To List  ${old_list}  ${contact}
+    Contact Lists Should Be Equal  ${old_list}  ${new_list}
+
+Delete contact
+    ${old_list}=  Get Contact List
+    ${len}=  Get Length  ${old_list}
+    ${index}=  Evaluate  random.randrange(${len})  random
+    ${contact}=  Get From List  ${old_list}  ${index}
+    Delete Contact  ${contact}
+    ${new_list}=  Get Contact List
+    Remove Values From List  ${old_list}  ${contact}
+    Contact Lists Should Be Equal  ${old_list}  ${new_list}
+
+Change contact name
+    ${old_list}=  Get Contact List
+    ${len}=  Get Length  ${old_list}
+    ${index}=  Evaluate  random.randrange(${len})  random
+    ${contact}=  Get From List  ${old_list}  ${index}
+    Change Contact Name  ${contact}  new_name101
+    ${new_list}=  Get Contact List
+    Contact Lists Should Be Equal  ${old_list}  ${new_list}
